e414bc2f776f0132f4aac4f27d17b276ffac5c4f
Jackiempty <candypaper0123@gmail.com>
2025-12-05 20:23:36 +0800

    feat: Finish 2-mmio-trap implementation

--- 2-mmio-trap/src/main/scala/riscv/core/InstructionFetch.scala
+++ 2-mmio-trap/src/main/scala/riscv/core/InstructionFetch.scala
@@ -66,8 +66,13 @@ class InstructionFetch extends Module {
     // - Inner multiplexer: Check jump flag
     //   - True: Use jump target address
     //   - False: Sequential execution
-    pc := ?
-
+    pc := Mux(io.interrupt_assert, io.interrupt_handler_address,
+      // Inner Mux: Check jump/branch (Second Priority)
+      Mux(io.jump_flag_id, io.jump_address_id,
+        // False: Sequential execution (Default)
+        pc + 4.U
+      )
+    )
   }.otherwise {
     // When instruction is invalid, hold PC and insert NOP (ADDI x0, x0, 0)
     // NOP = 0x00000013 allows pipeline to continue safely without side effects
